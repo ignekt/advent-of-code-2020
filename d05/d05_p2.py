f8b1a6646c80025d595fbbb72cfb634d4333967f
ignekt <ignekt@gmail.com>
2020-12-14 19:27:24 +0200

    Solve day 05

---
+++ d05/d05_p2.py
@@ -0,0 +1,27 @@
+def find_seat(descr):
+    start = 0
+    end = 127
+    for letter in descr[:7]:
+        if letter == 'F':
+            end = start + int((end-start)/2)
+        else:
+            start = start + int((end-start)/2) + 1
+    row = start
+
+    start = 0
+    end = 7
+    for letter in descr[7:]:
+        if letter == 'L':
+            end = start + int((end-start)/2)
+        else:
+            start = start + int((end-start)/2) + 1
+    column = start
+    return row * 8 + column
+
+f = open('input.txt')
+
+ids = [find_seat(line[:-1]) for line in f]
+
+for i in range(0, max(ids)+1):
+    if i not in ids:
+        print(i)
